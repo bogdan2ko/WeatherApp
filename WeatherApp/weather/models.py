041a4918a1a168b7b20f7009fa4dc97b3089c196
bogdan2ko1 <bogdanpogorelov7@gmail.com>
2023-03-03 20:31:53 +0100

    first commit

---
+++ WeatherApp/weather/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+class City(models.Model):
+    name=models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+    
+
+
+    # def save(self, *args, **kwargs):
+    #     if City.objects.count() < 10:
+    #         super().save(*args, **kwargs)
+    #     else:
+    #         pass
---
+++ WeatherApp/weather/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
+
+def index(request):
+    appid='bec8660e4f68fccfd04beece00aacd70'
+    url='https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='+ appid
+
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form=CityForm()
+
+    cities=City.objects.all()[::-1][:1]
+
+    last_object = City.objects.last()
+    if last_object: 
+        last_object.delete()
+        
+   
+   
+    all_cities=[]
+    try:
+        for city in cities:
+            res = requests.get(url.format(city.name)).json()
+            if res.get("main"):
+                city_info={
+                    'city':city.name,
+                    'temp':res["main"]["temp"],
+                    'icon':res["weather"][0]["icon"]
+                }
+            all_cities.append(city_info)
+    except:         
+        pass
+           
+
+        
+    context={'all_info':all_cities,'form':form}
+
+    return render (request, 'weather/index.html',context)
+
+
+
